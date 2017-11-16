c805d0ac62d0aaa13e4cf005abbf255c25edbf9f
Dan Osborne <dan.osborne@rocketmail.com>
2017-11-16 15:55:17 +0000

    New module added

--- src/prnx.js
+++ src/prnx.js
@@ -1,5 +1,6 @@
 import charlie from './components/charlie';
 import ermahgerd from './components/ermahgerd';
+import flatulence from './components/flatulence';
 import fullEnglish from './components/fullenglish';
 import placeholder from './components/placeholder';
 import randomiser from './components/randomiser';
@@ -8,11 +9,13 @@ import insertStyles from './shared/insert-styles';
 import draggable from './shared/draggable';
 
 insertStyles();
+flatulence();
 // draggable('img');
 
 const prnxMethods = {
  	charlie,
-	ermahgerd,
+    ermahgerd,
+    flatulence,
 	fullEnglish,
 	placeholder,
 }
